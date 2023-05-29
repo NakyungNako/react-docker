23a4484f278cadc022c6fb04d729b2e8f9ec8010
nam nguyen <phuongnam2804@gmail.com>
2023-05-29 14:12:00 +0700

    add submit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ function App() {
           Hello ReactJS!
         </h1>
         <p>Testing CircleCI - Docker CI/CD</p>
+        <form>
+          <label for="fname">First name:</label>
+          <input type="text" id="fname" name="fname"/><br/><br/>
+          <label for="lname">Last name:</label>
+          <input type="text" id="lname" name="lname"/><br/><br/>
+          <input type="submit" value="Submit"/>
+        </form>
       </header>
     </div>
   );
